1c7ad64f2851a3c8633450678a7521090f1f7ad3
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-05 23:57:44 +0530

    first commit

---
+++ Section One/Part One/Variables/var.cpp
@@ -0,0 +1,12 @@
+// A variable basically acts as a container that helps in storing data.
+
+// Data Types
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 10;
+    cout << age << endl;
+    cout<< sizeof(age) << endl;
+    return 0;
+}
---
+++ Section One/Part One/demo.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"DSA Series on C++\n";
+//     return 0;
+// }
+
+//Another format
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"DSA Series on C++ " << "Based on Programming"<< endl; //printing space between letters
+//     return 0;
+// }
