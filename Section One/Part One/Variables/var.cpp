031dd3ce981acf1a85c27376a47f31eb2aaed3ce
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-08 23:52:30 +0530

    Part One

--- Section One/Part One/Variables/var.cpp
+++ Section One/Part One/Variables/var.cpp
@@ -9,9 +9,15 @@ using namespace std;
 int main(){
     int age = 10;
     char grade = 'A';
+    float PI = 3.14f; // f is used to denote float literal
+    bool isAdult = true; // can be either true or false
     cout << age << endl;
     cout << "Size of Int is: " << sizeof(age) << endl;
     cout << grade << endl;
     cout << "Size of Char is: " << sizeof(grade) << endl;
+    cout << PI << endl;
+    cout << "Size of Float is: " << sizeof(PI) << endl;
+    cout << isAdult << endl;
+    cout << "Size of Bool is: " << sizeof(isAdult) << endl;
     return 0;
 }
