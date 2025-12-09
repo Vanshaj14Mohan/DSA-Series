0002e7397b6e0c51fa7763720b9ce49ecd0a541f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-09 22:49:00 +0530

    Part One

--- Section One/Part One/Variables/var.cpp
+++ Section One/Part One/Variables/var.cpp
@@ -1,8 +1,8 @@
 // A variable basically acts as a container that helps in storing data.
-
 // Data Types
 // 1. int - used to store integers (whole numbers) 4 bytes = 32 bits 
 // 2. char - used to store single characters 1 byte = 8 bits
+
 #include<iostream>
 using namespace std;
 
