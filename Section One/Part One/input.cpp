f6d6ada2e10205754b9738eb02c75fa7d0ac1648
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-16 23:09:28 +0530

    Part One

--- Section One/Part One/input.cpp
+++ Section One/Part One/input.cpp
@@ -9,17 +9,22 @@ int main(){
     cin >> age; // taking input using cin
     cout << "Your age is: " << age << endl;
 
-    // Method 2: Taking multiple inputs in a single line
-    // int a, b;
-    // cout << "Enter two numbers: ";
-    // cin >> a >> b; // taking multiple inputs using cin
-    // cout << "You entered: " << a << " and " << b << endl;
+    double price;
+    cout << "Enter the price: ";
+    cin >> price; // taking input using cin
+    cout << "The final price is: " << price << endl;
 
-    // // Method 3: Using getline to take string input with spaces
-    // string fullName;
-    // cout << "Enter your full name: ";
-    // cin.ignore(); // to ignore the newline character left in the buffer
-    // getline(cin, fullName); // taking string input with spaces
-    // cout << "Your full name is: " << fullName << endl;
+    // Method 2: Taking multiple inputs in a single line
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b; // taking multiple inputs using cin
+    cout << "You entered: " << a << " and " << b << endl;
+
+    // Method 3: Using getline to take string input with spaces
+    string fullName;
+    cout << "Enter your full name: ";
+    cin.ignore(); // to ignore the newline character left in the buffer
+    getline(cin, fullName); // taking string input with spaces
+    cout << "Your full name is: " << fullName << endl;
     return 0;
 }
