462e004bd3deb8824669e8d106fafd1c3b46338a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-21 16:30:10 +0530

    Part One

--- Section One/Part One/Exercises/five.cpp
+++ Section One/Part One/Exercises/five.cpp
@@ -13,3 +13,21 @@ int main(){
     cout << "Sum of Odd Numbers from 1 to 10 is: " << oddSum << endl;
     return 0;
 }
+
+//Using while loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n = 10;
+//     int sum = 0;
+//     int i = 1;
+//     while(i <= n){
+//         if (i%2 != 0){
+//             cout << i << endl;
+//             sum += i;
+//         }
+//         i++;
+//     }
+//     cout << "Sum of Odd numbes from 1 to N is: " << sum << endl;
+//     return 0;
+// }
