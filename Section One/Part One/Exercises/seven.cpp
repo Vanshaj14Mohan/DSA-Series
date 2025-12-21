3642a70573a271e20532fbaba3e42f30069acaa0
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-21 17:19:38 +0530

    Exercise

---
+++ Section One/Part One/Exercises/seven.cpp
@@ -0,0 +1,23 @@
+// 7: Wap to check whether a number is prime number or not
+#include<iostream>
+using namespace std;
+int main(){
+    bool isPrime = true;
+    int n;
+    cout << "Enter a number" << endl;
+    cin >> n; 
+    for (int i = 2; i<=n-1; i++){
+        if (n%i == 0){ //non-prime condition
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime == true){
+        cout << "Prime Number" << endl;
+    }
+    else{
+        cout << "Non Prime Number" << endl;
+    }
+    return 0; 
+
+}
