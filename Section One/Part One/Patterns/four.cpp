12a8116cd1bf0a370ed597cf8190824c2c0083ff
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-25 21:47:58 +0530

    Patterns

---
+++ Section One/Part One/Patterns/four.cpp
@@ -0,0 +1,21 @@
+// 4: Print another square pattern based on characters and the output should look like:
+// A B C
+// D E F
+// G H I
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    char ch = 'A';
+    cout << "Enter the character length: ";
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
