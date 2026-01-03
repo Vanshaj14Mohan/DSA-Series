3942e9e78fd062dfcf4a37746244280d51636877
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-03 23:21:23 +0530

    Part One

--- Section One/Part One/Functions/demo.cpp
+++ Section One/Part One/Functions/demo.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
 
+// A function must have a return type, and the function that does not return any value must be void
+// A Function just printing some part: Function Definition
+void printHello(){
+    cout << "Function Part\n";
+}
 
 int main(){
-    cout << "Hello Guys\n";
+    printHello(); // Calling that function;
+    printHello();
     int a = 5;
     int b = 10;
     cout << "Done\n";
