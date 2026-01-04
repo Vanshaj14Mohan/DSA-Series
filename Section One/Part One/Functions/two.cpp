c3112c76753aa2521d6e50c82c58cbf04905eba9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-04 23:43:34 +0530

    Part One

--- Section One/Part One/Functions/two.cpp
+++ Section One/Part One/Functions/two.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 //Function Declaration
-int calMin(int a, int b, int c){
+int calMin(int a, int b, int c){ //parameters can be said a copy of actual values we pass as arguments
     if(a<b && a<c){
         return a;
     }
@@ -15,7 +15,7 @@ int calMin(int a, int b, int c){
 }
 
 int main(){
-    int min = calMin(4,7,5); //Function Call and storing the value it returned
+    int min = calMin(4,7,5); //Function Call and storing the value it returned, arguments actual values passed to function
     cout << "Mininum number between three is :" << min <<endl; //Printing the value
     return 0;
 }
