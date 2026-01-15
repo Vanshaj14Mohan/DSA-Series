25c2605e8db9b3012e5008bd2ba1d3e188f95722
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-15 20:49:10 +0530

    Part One

---
+++ Section One/Part One/Functions/six.cpp
@@ -0,0 +1,28 @@
+// 6: Calculate nCr binomial coefficient for n and r using functions.
+// Best approach would be to create a factorial function and then use it to calclulate nCr
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    int fact = 1;
+    for(int i =1; i<=n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+// For binomial coefficient nCr => n! / r! * (n-r)!
+int nCr(int n, int r){
+    int num = fact(n);
+    int denom = fact(r) * fact(n-r);
+    return num/denom;
+}
+
+int main(){
+    int n, r;
+    cout << "enter n and r values:";
+    cin >> n >> r;
+    int res = nCr(n,r);
+    cout << "nCr value is: " << res << endl;
+    return 0;
+}
