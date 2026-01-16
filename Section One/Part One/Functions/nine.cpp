2ecdfec03886305bbef6cf462566431c982f653a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-16 22:56:01 +0530

    Part One

---
+++ Section One/Part One/Functions/nine.cpp
@@ -0,0 +1,28 @@
+// 9: Create a function to print nth Fibonaaci number
+#include<iostream>
+using namespace std;
+
+int fibonaaci(int n){
+    if (n == 0){
+        return 0;
+    }
+    if (n == 1){
+        return 1;
+    }
+    int a = 0, b = 1, c;
+    for(int i =2; i<=n; i++){
+        c = a+b;
+        a = b;
+        b =c;
+    }
+    return b;
+}
+
+int main(){
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+    int res = fibonaaci(n);
+    cout << "Nth fibonaaci number is: " << res << endl;
+    return 0;
+}
