bfcaf3edb7cf93b04e7631f9624060f0038ebd3a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-21 20:30:38 +0530

    Part One

---
+++ Section One/Part One/Binary Number System/bintodec.cpp
@@ -0,0 +1,21 @@
+// A simple code to convert into binary numbers into decimal numbers:
+#include<iostream>
+using namespace std;
+
+int bintodec(int n){
+    int ans = 0, pow = 1;
+    while(n > 0){
+        int rem = n%10;
+        ans += rem * pow;
+        n = n/10;
+        pow *= 2;
+    }
+    return ans;
+}
+
+int main(){
+    int n; 
+    cout << "Enter the number you want to convert into decimal: ";
+    cin >> n;
+    cout << n << " Entered number decimal value is " << bintodec(n) << endl;
+}
