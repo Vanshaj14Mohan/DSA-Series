1f3150eb301a0ceb3fecdb0f37724d48d161f5be
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-24 21:16:24 +0530

    Part One

---
+++ Section One/Part One/Bitwise Operators/bit_and.cpp
@@ -0,0 +1,10 @@
+// Checking how Bitwise & works
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b = 8;
+    cout << ( a & b) << endl; // Bitwise AND
+    cout << ( a | b) << endl; // Biwise OR would return 12
+    return 0;
+}
