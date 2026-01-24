ef63873389377adc308249411d1e6c7b7508f010
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-24 21:46:50 +0530

    Part One

--- Section One/Part One/Bitwise Operators/bit_and.cpp
+++ Section One/Part One/Bitwise Operators/bit_and.cpp
@@ -6,5 +6,7 @@ int main(){
     int a = 4, b = 8;
     cout << ( a & b) << endl; // Bitwise AND
     cout << ( a | b) << endl; // Biwise OR would return 12
+    cout << ( a ^ b) << endl; // Bitwise XOR would also return 12
+    cout << ( 5 << 1) << endl; // Using Bitwise Left Shift Operator n << i, output would be 10 of binary
     return 0;
 }
