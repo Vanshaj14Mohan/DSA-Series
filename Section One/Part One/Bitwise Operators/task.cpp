0fb819999d5f6b4ec2c835652bd6c8b2484895a2
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-01-30 16:09:37 +0530

    Part One

---
+++ Section One/Part One/Bitwise Operators/task.cpp
@@ -0,0 +1,19 @@
+// 1: Write a program and check if a number is power of 2 or not:
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n > 0 && ((n & (n-1)) == 0)){
+        cout << "Number is a power of 2";
+    }
+    else{
+        cout << "Number is not a power of 2";
+    }
+    return 0;
+}
+
+
+
