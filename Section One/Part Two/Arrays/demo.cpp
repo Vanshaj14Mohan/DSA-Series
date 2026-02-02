b956574adbd96e45debc3165558c56ea251333b5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-02 23:43:45 +0530

    Part Two

---
+++ Section One/Part Two/Arrays/demo.cpp
@@ -0,0 +1,10 @@
+// A normal program using arrays
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks[5] = {90, 92, 95, 93, 97}; // Array with 5 input values
+    double price[] = {100.50, 92.05, 94.24, 90.25}; // It automatically creates an array with 4 values 
+    cout << marks[0] << endl;
+    return 0;
+}
