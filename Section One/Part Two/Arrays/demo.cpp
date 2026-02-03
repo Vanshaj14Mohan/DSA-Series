fd223930efed8261a036af1bc29612316ecdf3a4
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-03 23:12:58 +0530

    Part Two

--- Section One/Part Two/Arrays/demo.cpp
+++ Section One/Part Two/Arrays/demo.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    // 0 to size-1
     int marks[5] = {90, 92, 95, 93, 97}; // Array with 5 input values
     double price[] = {100.50, 92.05, 94.24, 90.25}; // It automatically creates an array with 4 values 
     // Accesing elements
@@ -13,5 +14,8 @@ int main(){
     cout << price[0] << endl;
     cout << marks[2] << endl;
     cout << marks[3] << endl;
+    // we can also update it's value
+    marks[3] = 94;
+    cout << marks[3] << endl; // 94
     return 0;
 }
