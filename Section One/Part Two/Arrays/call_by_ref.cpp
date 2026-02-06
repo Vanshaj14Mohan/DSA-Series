f2006c6b6b9005938d4c91c857d3be5c01700b65
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-06 23:21:55 +0530

    Part Two

---
+++ Section One/Part Two/Arrays/call_by_ref.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//Creating a function 
+void change_arr(int arr[], int size){
+    cout << "In function: " << endl;
+    for(int i =0; i< size; i++){
+        arr[i] = 2* arr[i];
+    }
+}
+
+int main(){
+    int arr[] = {1,2,3};
+    change_arr(arr, 3);
+    cout << "In main function: " << endl;
+    for(int i =0; i< 3; i++){
+        cout << arr[i] << " "; // arr is passed by reference so whatever changes we make in function would reflect here as well
+    }
+    cout <<endl;
+    return 0;
+}
