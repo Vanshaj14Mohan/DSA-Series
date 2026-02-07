4214b354ad6035bc608b582e5a849c054bc97281
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-07 23:52:05 +0530

    Part Two

--- Section One/Part Two/Arrays/rev_arr.cpp
+++ Section One/Part Two/Arrays/rev_arr.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 
-// Making a function to reverse the array
+// Making a function to reverse the array, void as it's not returning anything, just modifying the original array:
 void rev_arr(int arr[], int size){
     int start = 0;
     int end = size-1;
@@ -16,12 +16,12 @@ void rev_arr(int arr[], int size){
 int main(){
     int arr[] = {1, 2, 3, 4, 5, 6};
     int size = sizeof(arr)/sizeof(arr[0]);
-    cout << "Original array is:" << endl;
+    cout << "Original array is:" << endl; // Printing original array
     for(int i =0; i< size; i++){
         cout << arr[i] << " ";
     }
     rev_arr(arr, size);
-    cout << "\nReversed array is : " << endl;
+    cout << "\nReversed array is : " << endl; // Printing reversed array, \n for new line
     for(int i =0; i< size; i++){
         cout << arr[i] << " ";
     }
