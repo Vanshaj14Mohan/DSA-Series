4f4a10a3af74e746042464f730558f3dc77aeb4d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-10 16:15:25 +0530

    Part Two

--- Section One/Part Two/Arrays/ques6.cpp
+++ Section One/Part Two/Arrays/ques6.cpp
@@ -1,1 +1,33 @@
 // 6: Write a program to print all the unique elements present in an array:
+#include<iostream>
+using namespace std;
+
+// Creating a  function to print unique elements 
+void unique_array(int arr[], int size){
+    int count = 0; // to count the repetition of each element;
+    cout << "Unique elements in the array are: " << endl;
+    for(int i =0; i<size; i++){
+        count = 0; // reset count for each element;
+        for(int j= 0; j< size; j++){
+            if(arr[i] == arr[j]){
+                count++;  // count repetition of each element
+            }
+        }
+        if(count == 1){ // means element is unique,
+            cout << arr[i] << " ";
+        }
+    }
+}
+
+// Main function 
+int main(){
+    int arr[] = {10, 20, 30, 20, 30, 9, 9, 5, 9};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "Original array is: " << endl;
+    for(int i =0; i< size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    unique_array(arr, size);
+    return 0;
+}
