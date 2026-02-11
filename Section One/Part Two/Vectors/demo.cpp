90a3418f2a3e0f1511078a904e9484d1d916bdb4
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-11 22:27:35 +0530

    Part Two

---
+++ Section One/Part Two/Vectors/demo.cpp
@@ -0,0 +1,21 @@
+// Vectors Introduction
+#include<iostream>
+#include<vector> // Header files for vectors
+using namespace std;
+
+int main(){
+    // Now creating a vector for integers:
+    // Type 1: 
+    vector<int> v; // Vector created
+    // cout << v[0] << endl;
+    // 2nd Type of creating a vector
+    vector <int> vec={1,2,3,4,5}; // Created and initialized a vector
+    cout << vec[0] << endl; // 1;
+    // 3rd Type 
+    vector<int> vect(4,0); // 4 represent size of array, 0 represent value of all elements in vector
+    cout <<vect[0]<<endl; // 0
+    cout <<vect[1]<<endl; // 0
+    cout <<vect[2]<<endl; // 0
+    cout <<vect[3]<<endl; //0
+    return 0;
+}
