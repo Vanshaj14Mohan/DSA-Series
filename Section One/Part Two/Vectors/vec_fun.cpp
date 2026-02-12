4cb9177c3093d7f8db7bd6441aa3ad46f6e44ccb
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-12 23:52:19 +0530

    Part Two

--- Section One/Part Two/Vectors/vec_fun.cpp
+++ Section One/Part Two/Vectors/vec_fun.cpp
@@ -4,12 +4,16 @@
 using namespace std;
 
 int main(){
-    // Checking size of an vector
+    // 1: Checking size of an vector
     vector<int> vec={1, 2, 3, 4, 5};
     cout << "Size of vector => " << vec.size() << endl; // 5
-    // Pushing back an element in vector
+    // 2: Pushing back an element in vector
     cout << "Pushing 6 at the back of vector" << endl;
     vec.push_back(6);
     cout << vec.size() << endl; //6
+    // 3: 
+    for (int i: vec){
+        cout << i << " ";
+    }
     return 0;
 }
