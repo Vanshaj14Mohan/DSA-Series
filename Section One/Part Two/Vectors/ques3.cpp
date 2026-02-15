d160e13084982ff883a64b9d650a2e87e2940745
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-15 23:51:42 +0530

    Part Two

---
+++ Section One/Part Two/Vectors/ques3.cpp
@@ -0,0 +1,26 @@
+// Find largest sub-array sum using vector and function
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxsubArray(vector<int> vec, int size){
+    int st = 0;
+    for(st =0; st< size; st++){
+        for(int end = st; end< size; end++){
+            for(int i = st; i<=end; i++){
+                cout << vec[i] << " ";
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+int main(){
+    vector<int> vec = {1, 2, 3, 4, 5};
+    int size = vec.size();
+    int res = maxsubArray(vec, size);
+    cout << "All sub arrays are: " << res << endl;
+    return 0;
+}
