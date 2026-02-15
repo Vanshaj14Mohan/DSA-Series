17a9260254db9646b8e06c25815f94976118660a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-15 18:53:10 +0530

    Part Two

---
+++ Section One/Part Two/Vectors/ques.cpp
@@ -0,0 +1,27 @@
+// Write Linear search using vector and functions
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Creating a function 
+int linearFunction(vector<int> &nums, int target){
+    for(int i =0; i<nums.size(); i++){
+        if(nums[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {4, 2, 5, 7, 8, 10, 11, 12};
+    int target = 10;
+    int res = linearFunction(nums, target);
+    if (res != -1){
+        cout << "Element found at index: " << res << endl;
+    }
+    else{
+        cout << "Element not found:";
+    }
+    return 0;
+}
