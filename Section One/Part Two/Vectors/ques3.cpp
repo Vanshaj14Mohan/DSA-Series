4d8f8454c80767232353d3d5239bad6466652eb4
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-16 22:12:58 +0530

    Part Two

--- Section One/Part Two/Vectors/ques3.cpp
+++ Section One/Part Two/Vectors/ques3.cpp
@@ -4,39 +4,23 @@
 #include<climits>
 using namespace std;
 
-// int maxsubArray(vector<int> vec, int size){
-//     for(int st =0; st< size; st++){
-//         for(int end = st; end< size; end++){
-//             for(int i = st; i<=end; i++){
-//                 cout << vec[i];
-//             }
-//             cout << " ";
-//         }
-//         cout << endl;
-//     }
-
-// }
-
-// int main(){
-//     vector<int> vec = {1, 2, 3, 4, 5};
-//     int size = vec.size();
-//     int res = maxsubArray(vec, size);
-//     cout << "All sub arrays are: " << res << endl;
-//     return 0;
-// }
-
-// using brute force approach
-int main(){
-    int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int maxSum = INT_MIN;
-    for(int st = 0; st<size; st++){
-        int currSum = 0;
-        for(int end= st; end< size; end++){
-            currSum += arr[end];
-            maxSum = max(currSum, maxSum);
+int maxsubArray(vector<int> vec, int size){
+    for(int st =0; st< size; st++){
+        for(int end = st; end< size; end++){
+            for(int i = st; i<=end; i++){
+                cout << vec[i];
+            }
+            cout << " ";
         }
+        cout << endl;
     }
-    cout << "Max subarray sum is: " << maxSum << endl;
+
+}
+
+int main(){
+    vector<int> vec = {1, 2, 3, 4, 5};
+    int size = vec.size();
+    int res = maxsubArray(vec, size);
+    cout << "All sub arrays are: " << res << endl;
     return 0;
 }
