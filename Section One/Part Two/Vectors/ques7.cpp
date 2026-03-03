62c3a1949c6650189c00f928de7ca12308a166ec
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-03 23:56:15 +0530

    Part Two

--- Section One/Part Two/Vectors/ques7.cpp
+++ Section One/Part Two/Vectors/ques7.cpp
@@ -5,6 +5,24 @@ using namespace std;
 
 // Making a function
 
+int majorelem(vector<int> &vec){
+    int n = vec.size();
+    int freq = 0;
+    int ans = 0;
+    for(int i =0; i<n; i++){
+        if(freq == 0){
+            ans = vec[i];
+        }
+        if(ans == vec[i]){
+            freq++;
+        }
+        else{
+            freq--;
+        }
+    }
+    return ans;
+}
 int main(){
     vector<int> vec = {1, 2, 2, 1, 1, 1};
+    //Checking for majority element
 }
