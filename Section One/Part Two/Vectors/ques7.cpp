7aeef61e931c3a0e5a7c4f550624d39842684ca3
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-03 23:52:00 +0530

    Part Two

---
+++ Section One/Part Two/Vectors/ques7.cpp
@@ -0,0 +1,10 @@
+// Find the majority element present in an array
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Making a function
+
+int main(){
+    vector<int> vec = {1, 2, 2, 1, 1, 1};
+}
