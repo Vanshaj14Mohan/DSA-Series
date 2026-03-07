d5391fae96a963f5f39d950df18bc5314aa6e0e2
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-07 21:39:13 +0530

    Questions

--- Questions/stock.cpp
+++ Questions/stock.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 int maxProfit(int prices[], int n){
-    int bestBuy = prices[0];
+    int bestBuy = prices[0]; // assuming bestBuy starts from index 0
     int maxProfit = 0;
     for(int i = 1; i<n; i++){
-        if(prices[i]> bestBuy){
+        if(prices[i]> bestBuy){ 
             maxProfit = max(maxProfit, prices[i]-bestBuy);
         }
         bestBuy = min(bestBuy, prices[i]);
