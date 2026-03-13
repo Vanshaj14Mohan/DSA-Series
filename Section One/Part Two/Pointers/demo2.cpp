7e27e2f2645e2e4fbe64dc66a9ed732181227fc5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-13 23:36:39 +0530

    Part Two

--- Section One/Part Two/Pointers/demo2.cpp
+++ Section One/Part Two/Pointers/demo2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main(){
     int** ptr;
+    int *ptr2 = NULL;
     cout << ptr << endl; // it will show some garbage value as we didn't initialized it any value
+    cout << ptr2 << endl; // it will show 0, as we assigned it NULL ie 0
+
     return 0;
 }
