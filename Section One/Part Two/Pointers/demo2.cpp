fdbf21bf8e17357f17e6790e1f2128f0dc95367b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-13 23:45:16 +0530

    Part Two

--- Section One/Part Two/Pointers/demo2.cpp
+++ Section One/Part Two/Pointers/demo2.cpp
@@ -7,7 +7,8 @@ int main(){
     int** ptr;
     int *ptr2 = NULL;
     cout << ptr << endl; // it will show some garbage value as we didn't initialized it any value
-    cout << ptr2 << endl; // it will show 0, as we assigned it NULL ie 0
+    cout << ptr2 << endl; // it will show 0, as we assigned it NULL ie 0, memory address is null
+    // And we can't dereference a null pointer as it doesn't point to any valid memory location.
 
     return 0;
 }
