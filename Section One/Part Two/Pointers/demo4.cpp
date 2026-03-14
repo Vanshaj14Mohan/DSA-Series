6fff4639ca164562127273d3112aae7f8936c137
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-14 23:56:10 +0530

    Part Two

--- Section One/Part Two/Pointers/demo4.cpp
+++ Section One/Part Two/Pointers/demo4.cpp
@@ -9,6 +9,6 @@ int main(){
     int* ptr = &a;
     cout << ptr << endl; // 0x61ff08
     ptr++;
-    cout << ptr << endl; // 0x61ff0c ie (8 9 a b) hexadecimal stage
+    cout << ptr << endl; // 0x61ff0c ie (8 9 a b) 4 bytes space,  hexadecimal stage
     return 0;
 }
