5b7d466840372f0076a2f4c577e0d0ebcd54eee8
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-15 00:49:37 +0530

    Part Two

--- Section One/Part Two/Pointers/demo4.cpp
+++ Section One/Part Two/Pointers/demo4.cpp
@@ -13,5 +13,15 @@ int main(){
     ptr--;
     cout << ptr <<endl; // 0x61ff08, original one (c b a 9) 4 bytes space again
     // Add/Subtract a number in pointers
+    int b = 20;
+    int* str = &b;
+    cout << str << endl;
+    str = str + 2; // 2int => 8 Bytes
+    cout << str << endl;
+    int arr[] = {1, 2, 3, 4, 5};
+    cout << *arr << endl; // 1
+    cout << *(arr+1) <<endl; // 2
+    cout << *(arr+2) <<endl; // 3
+    cout << *(arr+3) <<endl; // 4
     return 0;
 }
