cb512a504558c22ce6d37ded5775557b794b583e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-18 00:47:33 +0530

    Part Two

---
+++ Questions/binary_search.cpp
@@ -0,0 +1,34 @@
+// A program to implement binary search
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// creating a function
+int bin_search(vector<int> arr, int target){ // Iterative code of binary search
+    int st = 0, end =  arr.size() - 1;
+    while(st <=end){
+        int mid = (st+end)/2;
+        // Case 1:
+        if(target > arr[mid]){
+            st = mid + 1;
+        }
+        // Case 2:
+        else if(target < arr[mid]){
+            end = mid-1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+
+}
+
+int main(){
+    vector<int> arr = {-1, 0, 3, 4, 5, 9, 12};
+    int target = 12;
+    int res = bin_search(arr, target);
+    cout << "Target element found at index " << res << endl;
+    return 0;
+}
