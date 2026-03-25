437513806c11f8b3e0a3799b2c84e5b765cf04f5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-25 23:57:36 +0530

    Questions

---
+++ Questions/peak_no.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int peak_no(vector<int> &arr){
+    int st = 1, end = arr.size()-2;
+    while(st<=end){
+        int mid = st+(end-st)/2;
+        if(arr[mid-1] < arr[mid] && arr[mid] >> arr[mid + 1]){
+            return mid;
+        }
+        // Else checking whether we are on right side or left side:
+        else if(arr[mid] < arr[mid -1]){ // right side
+            st = mid + 1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> arr = {0, 3, 8, 9, 5, 2};
+    int res = peak_no(arr);
+    cout << "Peak number is: " << res << endl;
+    return 0;
+}
